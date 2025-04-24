9b3368e15f01a0c1acf6ad8e874fe7622042417b
Jiahao, Woo <woojiahao1234@gmail.com>
2025-04-24 13:31:58 +0800

    Add basic components

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import survey from "@/data/survey_2024.json"
+import { useMemo } from "react"
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { FaTriangleExclamation } from "react-icons/fa6";
+import { columns, SurveyEntry } from "@/survey/columns";
+import { DataTable } from "./components/ui/data-table";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const surveyData: SurveyEntry[] = useMemo(() => survey.map(entry => entry as SurveyEntry), []);
+  const years = useMemo(() => {
+    const distinctYears = new Set(surveyData.map(entry => entry.year));
+    const sortedYears = Array.from(distinctYears).sort();
+    return sortedYears;
+  }, [surveyData]);
+
+  const universities = useMemo(() => {
+    const distinctUniversities = new Set(surveyData.map(entry => entry.university));
+    const sortedUniversities = Array.from(distinctUniversities).sort();
+    return sortedUniversities;
+  }, [surveyData]);
+
+  const schools = useMemo(() => {
+    const distinctSchools = new Set(surveyData.map(entry => entry.school));
+    const sortedSchools = Array.from(distinctSchools).sort();
+    return sortedSchools;
+  }, [surveyData]);
+
+  const degree = useMemo(() => {
+    const distinctDegrees = new Set(surveyData.map(entry => entry.degree));
+    const sortedDegrees = Array.from(distinctDegrees).sort();
+    return sortedDegrees;
+  }, [surveyData]);
+
+  console.log(years)
+  console.log(universities)
+  console.log(schools)
+  console.log(degree)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="container my-12 mx-auto">
+      <Alert>
+        <FaTriangleExclamation />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          The salary should not be the only metric you use to decide whether you join a major! You should consider your interest and alignment with the content taught.
+        </AlertDescription>
+      </Alert>
+
+      <DataTable columns={columns} data={surveyData} />
+    </div>
   )
 }
 
