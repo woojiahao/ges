98523682555efb75f603bd4a89ab283a3efdbd11
Jiahao, Woo <woojiahao1234@gmail.com>
2025-05-06 20:22:20 +0800

    Change survey data table to card

--- src/components/survey/data-table.tsx
+++ src/components/survey/data-table.tsx
@@ -4,6 +4,7 @@ import { FilterDropdown } from "@/components/survey/filter-dropdown";
 import { multiSelectFilterFn } from "@/components/survey/filters";
 import { YearRangeInput } from "@/components/survey/year-range-input";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { SurveyEntry } from "@/data/survey-entry";
 import { ColumnFiltersState, flexRender, getCoreRowModel, getFilteredRowModel, getPaginationRowModel, useReactTable, VisibilityState } from "@tanstack/react-table";
@@ -78,78 +79,84 @@ export function SurveyDataTable({
   })
 
   return (
-    <div>
-      <div className="flex justify-between items-center py-4">
-        <div className="flex flex-row gap-4">
-          <YearRangeInput table={table} years={years} />
-          <FilterDropdown table={table} columnId="university" available={universities} />
-          <FilterDropdown table={table} columnId="school" available={schools} />
-          <FilterDropdown table={table} columnId="degree" available={degrees} />
+    <Card>
+      <CardHeader>
+        <CardTitle>Survey Data</CardTitle>
+        <CardDescription>View the results of the GES survey from 2013 to 2023</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="flex justify-between items-center py-4">
+          <div className="flex flex-row gap-4">
+            <YearRangeInput table={table} years={years} />
+            <FilterDropdown table={table} columnId="university" available={universities} />
+            <FilterDropdown table={table} columnId="school" available={schools} />
+            <FilterDropdown table={table} columnId="degree" available={degrees} />
+          </div>
+          <ColumnVisibilityDropdown table={table} />
         </div>
-        <ColumnVisibilityDropdown table={table} />
-      </div>
-      <div className="rounded-md border">
-        <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                    </TableHead>
-                  )
-                })}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                    </TableCell>
-                  ))}
+        <div className="rounded-md border">
+          <Table>
+            <TableHeader>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    return (
+                      <TableHead key={header.id}>
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                      </TableHead>
+                    )
+                  })}
                 </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell colSpan={table.getAllColumns().length} className="h-24 text-center">
-                  No results.
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </div>
-      <div className="flex items-center justify-end space-x-2 py-4">
-        <Button
-          variant="outline"
-          size="sm"
-          onClick={() => table.previousPage()}
-          disabled={!table.getCanPreviousPage()}
-        >
-          Previous
-        </Button>
-        <Button
-          variant="outline"
-          size="sm"
-          onClick={() => table.nextPage()}
-          disabled={!table.getCanNextPage()}
-        >
-          Next
-        </Button>
-      </div>
-    </div>
+              ))}
+            </TableHeader>
+            <TableBody>
+              {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && "selected"}
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell colSpan={table.getAllColumns().length} className="h-24 text-center">
+                    No results.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
+        <div className="flex items-center justify-end space-x-2 py-4">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
   )
 }
